85becbef48439ba75b2ccc988361082e7a648f67
aaazlkm <40765910+aaazlkm@users.noreply.github.com>
2022-04-03 14:26:25 +0900

    c++環境構築

---
+++ c++/sample.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World" << endl;
+  std::vector<int64_t> v = {0, 100, 20000};
+  std::cout << v[1] << std::endl; //=> 100
+  return 0;
+}
